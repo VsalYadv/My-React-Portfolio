9c89dbc8479a3d538fd059818cb3ecef5a38f69f
Vsal Yadv <vsalyadv117@gmail.com>
2023-07-18 03:29:24 -0700

    change Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Pete • © 2022</p>
+          <p>Vishal Yadav • © 2023 Copyright</p>
         </Flex>
       </footer>
     </Box>
